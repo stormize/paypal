e3cf6e515c5d66b885b627fc16f8fe04c0f772bc
amr makram <amr.makram2@gmail.com>
2019-04-23 17:53:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser');
 
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
-  'client_id': 'AaU8tQfmz1_MFDTKuf84yYERXvdDt2ZFJVrxhNW_49DazF4A_F0VBuKyV5_nntyEdZqUa5Oq9ZBj65GV',
-  'client_secret': 'EAZ8aFDU4lHHLy1bQqULYWqznf3dBknXZW3AH__zFC0bUs8AGUyR6RNbm-jHvqtikX7PsSqMO5vxuvKm'
+  'client_id': 'AQ12lUmTkZyrazIDt_JQQwXa531tj31yNfWsxcG44OC0ssSreWQZKdIwsj90ews8-xBbaUINExFzGweG',
+  'client_secret': 'EMmb1AxcOepeVuTEPKlu-gy-atlAp5GCKp9Xknj2SMVLd9ci9lGD8Nnw3fSRsMoZyca0W4y9N5aZmjV8'
 });
 
 const app = express();
